dccab39a1956b8137cc8662bec59ae18c3b14932
brentphilion <43643086+brentphilion@users.noreply.github.com>
2021-10-29 08:31:28 -0400

    building comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
@@ -14,3 +15,12 @@ class PostAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(models.Post, PostAdmin)
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
